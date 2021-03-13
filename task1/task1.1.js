0c773440bdfea410a6257842a96e71ba7de29fe4
Fomenko Gregory <fmk.g@ya.ru>
2021-03-13 16:57:22 +0300

    feat: task 1.1

---
+++ task1/task1.1.js
@@ -0,0 +1,10 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+rl.on("line", (input) => {
+  console.log(input.split("").reverse().join(""));
+  console.log(Array(input.length).fill("-").join(""));
+});
