c9a887844a16babfb8d75998ff3836ab3b02b58f
Fomenko Gregory <fmk.g@ya.ru>
2021-04-03 17:28:22 +0300

    feat: Sequelize connection

--- task2/index.ts
+++ task2/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+import { Validator } from './validator';
 import { UsersDatabase } from './users.database';
 import { UsersController } from './users.controller';
 import { userPartialSchema, userRequiredSchema } from './user.schemas';
-import { Validator } from './validator';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 3000;
@@ -25,3 +29,21 @@ app.route(`${controller.PREFIX}/search`).get(controller.getAutoSuggestUsers);
 app.listen(port, () => {
     console.log(`App is listening at http://localhost:${port}`);
 });
+
+new Sequelize(process.env.DATABASE_URL ?? '', {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+})
+    .authenticate()
+    .then(() =>
+        console.log('Sequelize: connection has been established successfully.')
+    )
+    .catch((error) =>
+        console.error('Sequelize: unable to connect to the database:', error)
+    );
